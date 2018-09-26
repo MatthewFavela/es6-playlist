1c0b23f11318e801d7c043f217b48614f4af1a43
MatthewFavela <mfavela3508@myhta.org>
2018-09-26 12:09:10 -1000

    update

--- data.js
+++ data.js
@@ -1,40 +1,152 @@
 const data = [
     {
       artist: 'Travis Scott',
-      genre: 'Chillin',
+      genre: 'Hip-Hop/Rap',
       song: 'Stargazing',
       image: 'https://is1-ssl.mzstatic.com/image/thumb/Music118/v4/72/5a/e4/725ae48d-4405-d4bd-0694-91a1b16035d6/886447227169.jpg/268x0w.jpg'
     },
     {
         artist: 'Mac Miller',
-        genre: 'Sad boyo hours',
+        genre: 'Hip-Hop/Rap',
         song: 'Come Back to Earth',
         image: 'https://images.genius.com/0327e4a856f14b2430e6e1a9333b1f1f.1000x1000x1.jpg'
       },
       {
         artist: 'Young Dolph',
-        genre: 'Rap',
+        genre: 'Hip-Hop/Rap',
         song: 'By Mistake',
         image: 'https://t2.genius.com/unsafe/220x220/https%3A%2F%2Fimages.genius.com%2Fb0813223ada5674f0f01ad324ff22cf2.750x750x1.jpg'
       },
       {
         artist: 'Lil Skies',
-        genre: 'Work Music',
+        genre: 'Hip-Hop/Rap',
         song: 'Big Money',
         image: 'https://i.ytimg.com/vi/4_Y69NlDTBY/maxresdefault.jpg'
       },
       {
         artist: 'YBN Nahmir',
-        genre: 'Squad Music',
+        genre: 'Hip-Hop/Rap',
         song: 'Up-Top Baby',
         image: 'https://i.ytimg.com/vi/OFe58QHZHMY/maxresdefault.jpg'
       },
       {
         artist: 'Wiz Khalifa',
-        genre: 'Laying in Bed being Bored',
+        genre: 'Hip-Hop/Rap',
         song: 'Hopeless Romantic',
-        image: 'https://stupiddope.com/wp-content/uploads/2018/06/maxresdefault-1.jpg'
+        image: 'https://is1-ssl.mzstatic.com/image/thumb/Music118/v4/72/5a/e4/725ae48d-4405-d4bd-0694-91a1b16035d6/886447227169.jpg/268x0w.jpg'
       },
+      {
+        artist: 'Jason Aldeen',
+        genre: 'Country',
+        song: 'Big Green Tractor',
+        image: 'https://i.ytimg.com/vi/cjwAucpiC6Q/hqdefault.jpg'
+      },
+      {
+        artist: 'Lil Tracy x Lil Uzi Vert',
+        genre: 'Country',
+        song: 'Like A Farmer',
+        image: 'https://i1.sndcdn.com/artworks-000349668423-mhizrt-t500x500.jpg'
+      },
+      {
+        artist: 'John Denver',
+        genre: 'Country',
+        song: 'West Virginia / Mountain Momma',
+        image: 'https://c2.staticflickr.com/4/3183/3022527876_40136ed79f_z.jpg?zz=1'
+      },
+      {
+        artist: 'Hanna Montana',
+        genre: 'Country',
+        song: 'Hoedown Throwdown',
+        image: 'http://static.tvtropes.org/pmwiki/pub/images/hm_7592.png'
+      },
+      {
+        artist: 'Florida Georgia line',
+        genre: 'Country',
+        song: 'Simple',
+        image: 'https://www.cropscience.bayer.com/-/media/bcs-inter/ws_globalportal/crop-science/smallholder-farming/smallholder-farming-women-in-africa-women-in-agriculture.jpg'
+      },
+      {
+        artist: 'Team America',
+        genre: 'Country',
+        song: 'Merica! Heck yea',
+        image: 'https://i.ytimg.com/vi/KtJJC0nWs9s/maxresdefault.jpg'
+      },
+      {
+        artist: 'Luis Fonsi',
+        genre: 'Hispanic',
+        song: 'Despacito',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61sw1j2fR%2BL._SS500.jpg'
+      },
+      {
+        artist: '6ix9ine',
+        genre: 'Hispanic',
+        song: 'BEBE',
+        image: 'https://i.ytimg.com/vi/_9wGRaQxZL8/hqdefault.jpg'
+      },
+      {
+        artist: 'Prodigy.exe',
+        genre: 'Hispanic',
+        song: 'Despacito 2',
+        image: 'https://i.kym-cdn.com/entries/icons/mobile/000/026/318/bec_1_.jpg'
+      },
+      {
+        artist: 'XXXTentacion',
+        genre: 'Hispanic',
+        song: 'I dont even speak spanish lol',
+        image: 'https://i.ytimg.com/vi/HjWsRYUKdS8/maxresdefault.jpg'
+      },
+      {
+        artist: 'Luis Enrique',
+        genre: 'Hispanic',
+        song: 'Amiga',
+        image: 'https://i.ytimg.com/vi/WsFrDruqcn8/maxresdefault.jpg'
+      },
+      {
+        artist: 'Los Tigres del Norte',
+        genre: 'Hispanic',
+        song: 'El Caballo Chamacuz',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61LuXDCXYfL._SS500.jpg'
+      },
+      {
+        artist: 'Cardi B',
+        genre: 'R&B',
+        song: 'I like it',
+        image: 'https://images.genius.com/9f92b837efe7107322fffb59f4699068.1000x1000x1.jpg'
+      },
+      {
+        artist: 'Drake',
+        genre: 'R&b',
+        song: 'Gods Plan',
+        image: 'https://direct.rhapsody.com/imageserver/images/Alb.297575682/500x500.jpg'
+      },
+      {
+        artist: 'Khalid',
+        genre: 'R&B',
+        song: 'OTW',
+        image: 'https://i.ytimg.com/vi/hjf-h6NMj7U/maxresdefault.jpg'
+      },
+      {
+        artist: 'Donald Glover',
+        genre: 'R&B',
+        song: 'This is America',
+        image: 'https://i.ytimg.com/vi/LghECy8jpp0/maxresdefault.jpg'
+      },
+      {
+        artist: 'Juice Wrld',
+        genre: 'R&B',
+        song: 'Lucid Dreams',
+        image: 'https://i.ytimg.com/vi/_fh64GbFSw4/maxresdefault.jpg'
+      },
+
   ]  
+
+
+
+
+
+
+
+
+
 
 
