beb4e699bd37123d8b7bc59369d3374229acd7ca
MatthewFavela <mfavela3508@myhta.org>
2018-09-26 11:08:30 -1000

    update

---
+++ data.js
@@ -0,0 +1,40 @@
+const data = [
+    {
+      artist: 'Travis Scott',
+      genre: 'Chillin',
+      song: 'Stargazing',
+      image: 'https://is1-ssl.mzstatic.com/image/thumb/Music118/v4/72/5a/e4/725ae48d-4405-d4bd-0694-91a1b16035d6/886447227169.jpg/268x0w.jpg'
+    },
+    {
+        artist: 'Mac Miller',
+        genre: 'Sad boyo hours',
+        song: 'Come Back to Earth',
+        image: 'https://images.genius.com/0327e4a856f14b2430e6e1a9333b1f1f.1000x1000x1.jpg'
+      },
+      {
+        artist: 'Young Dolph',
+        genre: 'Rap',
+        song: 'By Mistake',
+        image: 'https://t2.genius.com/unsafe/220x220/https%3A%2F%2Fimages.genius.com%2Fb0813223ada5674f0f01ad324ff22cf2.750x750x1.jpg'
+      },
+      {
+        artist: 'Lil Skies',
+        genre: 'Work Music',
+        song: 'Big Money',
+        image: 'https://i.ytimg.com/vi/4_Y69NlDTBY/maxresdefault.jpg'
+      },
+      {
+        artist: 'YBN Nahmir',
+        genre: 'Squad Music',
+        song: 'Up-Top Baby',
+        image: 'https://i.ytimg.com/vi/OFe58QHZHMY/maxresdefault.jpg'
+      },
+      {
+        artist: 'Wiz Khalifa',
+        genre: 'Laying in Bed being Bored',
+        song: 'Hopeless Romantic',
+        image: 'https://stupiddope.com/wp-content/uploads/2018/06/maxresdefault-1.jpg'
+      },
+  ]  
+
+
